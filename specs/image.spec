380fd0c8ddf60b7a1c4feef73c6f7fcae8a59434
coocoder <delaneyb@fullsixuk.com>
2015-11-27 09:15:42 +0000

    spike - raise exceptions if outside of max and min

--- specs/image.spec
+++ specs/image.spec
@@ -1,16 +1,12 @@
 Image Specification
 ===================
 
-
 An image can be a maximum of 250 pixels in width
 An image can be a maximum  of 250 pixels in height
-
-
-Create valid images
----------------------
 
 * Create images
   |x  |y  |pixel count  |Exception         |
   |---|---|-------------|------------------|
   |3  |3  |9            |none              |
   |250|250|62500        |none              |
+  
--- step_implementations/steps.rb
+++ step_implementations/steps.rb
@@ -8,27 +8,30 @@ class InvalidWidthError < StandardError;end
 class InvalidHeightError < StandardError;end
 
 class Image
-  attr_reader :x,:y,:pixels
-  def initialize(x,y)s
-    @x = x
-    @y = y
+  MAXIMUM_HEIGHT=250
+  MAXIMUM_WIDTH=250
+  MINIMUM_HEIGHT=1
+  MINIMUM_WIDTH=1
+  attr_reader :width,:height,:pixels
+  def initialize(width,height)
+    @width = width
+    @height = height
     @pixels = []
 
-    if valid?
-      for i in  1..x do
-        for j in 1..y do
-          @pixels << Pixel.new(self,i,j)
-        end
+    raise InvalidWidthError.new("#{@width} is greater than the maximum width of #{MAXIMUM_WIDTH}") if self.x > MAXIMUM_WIDTH
+    raise InvalidHeightError.new("#{@height} is greater than the maximum height of #{MAXIMUM_HEIGHT}") if self.y > MAXIMUM_HEIGHT
+    raise InvalidWidthError.new("#{@width} is less than the minimum width of #{MINIMUM_WIDTH}") if self.x < MINIMUM_WIDTH
+    raise InvalidHeightError.new("#{@height} is greater than the minimum height of #{MINIMUM_HEIGHT}") if self.y < MINIMUM_HEIGHT
+
+    for i in  1..x do
+      for j in 1..y do
+        @pixels << Pixel.new(self,i,j)
       end
     end
+
+
   end
-  def valid?
-    return InvalidWidthError if self.x > 250
-    return InvalidHeightError if self.y > 250
-    return InvalidWidthError if self.x < 1
-    return InvalidHeightError if self.y < 1
-    return true
-  end
+
 end
 
 class Pixel
