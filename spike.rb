59c3ce31d363703f69636afa19f42c6411dda0d1
coocoder <delaneyb@fullsixuk.com>
2015-11-29 03:58:41 +0000

    now with method for working out region sides

--- spike.rb
+++ spike.rb
@@ -1,57 +1,93 @@
-bitmap = BitMap.new
+  def fill(region,x,y,width,height)
 
-# show contents
-image = ImageBuilder.new(5,6) do
-  l(2,3,A)
-  s
-end
+    puts "width: #{width}"
+    puts "height: #{height}"
+    puts "x: #{x}"
+    puts "Y: #{y}"
 
-image = ImageBuilder.new
-  .i(5,6)
-  .l(2,3,A)
-  .s()
+    top = lambda{ |x,y,height|
+      return x,(y-1)
+    }
 
-#clear the image
-bitmap
-  .i(5,6)
-  .c()
+    bottom = lambda{ |x,y,height|
+      return x,(y+1)
+    }
 
-#paint the pixel
-bitmap
-  .i(5,6)
-  .l(2,3,A)
-  .s()
+    left = lambda{ |x,y,height|
+      return (x-1),y
+    }
 
-#paint vertically
-bitmap
-  .i(5,6)
-  .v(4,1,6,W)
-  .s()
+    right = lambda{ |x,y,height|
+      return (x+1),y
+    }
 
-#paint horizontally
-bitmap
-  .i(5,6)
-  .h(4,1,6,W)
-  .s()
+    puts "top: #{top.call(x,y,height)}"
+    puts "right: #{right.call(x,y,height)}"
+    puts "bottom: #{bottom.call(x,y,height)}"
+    puts "left: #{left.call(x,y,height)}"
 
-#things to remember
+    region.each do |p|
+      fill(region,p[0],p[1],width,height)
+    end
+  end
 
-* all pixels must be within the bounds of the image which is determined by the size of the image
-defined by the i call
+  region=[]
+  fill(region,2,2,3,3)
 
-* would be better reuse the l method in the vertical and horizontal
-
-* we will neeed to create a parser for the input data that calls the right methods.
-the parser must read line by line.
-
-* the first method that gets called is the I and if not called we should raise an error
-* an image is passed from function to function. an image is a collection of 1 more pixels
-
-* a pixel is x,y,color,image and , x and y must be between 1 - 250
-* a pixel must be with in the bounds of the image x and y values.
-* the horizontal and vertical methods create the collection of cordinates and then pass them to
-the l method.
-* the method first checks that the cordinate is in the bounds of the image and then updates the colour
-value , else it throws an out of bound error.
-
-* add exception handling for out of bounds
+# bitmap = BitMap.new
+#
+# # show contents
+# image = ImageBuilder.new(5,6) do
+#   l(2,3,A)
+#   s
+# end
+#
+# image = ImageBuilder.new
+#   .i(5,6)
+#   .l(2,3,A)
+#   .s()
+#
+# #clear the image
+# bitmap
+#   .i(5,6)
+#   .c()
+#
+# #paint the pixel
+# bitmap
+#   .i(5,6)
+#   .l(2,3,A)
+#   .s()
+#
+# #paint vertically
+# bitmap
+#   .i(5,6)
+#   .v(4,1,6,W)
+#   .s()
+#
+# #paint horizontally
+# bitmap
+#   .i(5,6)
+#   .h(4,1,6,W)
+#   .s()
+#
+# #things to remember
+#
+# * all pixels must be within the bounds of the image which is determined by the size of the image
+# defined by the i call
+#
+# * would be better reuse the l method in the vertical and horizontal
+#
+# * we will neeed to create a parser for the input data that calls the right methods.
+# the parser must read line by line.
+#
+# * the first method that gets called is the I and if not called we should raise an error
+# * an image is passed from function to function. an image is a collection of 1 more pixels
+#
+# * a pixel is x,y,color,image and , x and y must be between 1 - 250
+# * a pixel must be with in the bounds of the image x and y values.
+# * the horizontal and vertical methods create the collection of cordinates and then pass them to
+# the l method.
+# * the method first checks that the cordinate is in the bounds of the image and then updates the colour
+# value , else it throws an out of bound error.
+#
+# * add exception handling for out of bounds
